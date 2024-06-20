97aab7505e5fc948b8ec9eaf7e52ed61d7c4d16f
Dhiraj Mahapatro <mahadhir@amazon.com>
2024-06-20 19:43:15 -0400

    Optimize code

--- tool-use-with-stepfunctions/functions/check-inventory/app.mjs
+++ tool-use-with-stepfunctions/functions/check-inventory/app.mjs
@@ -1,24 +1,8 @@
 export const handler = async (event) => {
-    console.log(event);
-    const { content, role } = event.Body;
-    const { id, input } = event.Body.content[1];
-    const inventoryPresent = (input.carModel === '2015 Audi A7');
-    const messages = [];
-    
-    messages.push({
-      role,
-      content
-    });
-    
-    messages.push({
-      role: "user",
-      content: [{
-        "type": "tool_result",
-        "tool_use_id": id,
-        "content": [{type: "text", "text": JSON.stringify({ inventoryPresent })}],
-      }]
-    });
-  
-    return messages;
+  const { input: { carModel } } = event;
+  const targetModel = '2015 Audi A7';
+
+  return {
+    inventoryPresent: carModel === targetModel
   };
-  
+};
--- tool-use-with-stepfunctions/functions/schedule-appointment/app.mjs
+++ tool-use-with-stepfunctions/functions/schedule-appointment/app.mjs
@@ -1,28 +1,10 @@
 export const handler = async (event) => {
-    const { content, role } = event.Body;
-    const { id, input: { appointmentType, carModel } } = event.Body.content[1];
-    const messages = [];
-    
-    let tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    
-    messages.push({
-      role,
-      content
-    });
-  
-    messages.push({
-      role: "user",
-      content: [{
-        "type": "tool_result",
-        "tool_use_id": id,
-        "content": [{
-          type: "text", 
-          "text": `Appointment for your ${carModel} has been set on ${tomorrow.toDateString()} for ${appointmentType} `
-        }],
-      }]
-    });
-    
-    return messages;
+  const { appointmentType, carModel } = event;
+
+  let tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  return {
+    "appointment": `Appointment for your ${carModel} has been set on ${tomorrow.toDateString()} for ${appointmentType}`
   };
-  
+};

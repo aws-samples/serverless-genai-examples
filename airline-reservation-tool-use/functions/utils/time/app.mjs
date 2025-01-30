64b7ef22079cca49b55f8fbf7e2b394e9bc11010
Dhiraj Mahapatro <mahadhir@amazon.com>
2025-01-29 19:30:20 -0500

    Use Nova Pro instead of Claude Sonnet 3.

--- airline-reservation-tool-use/functions/utils/time/app.mjs
+++ airline-reservation-tool-use/functions/utils/time/app.mjs
@@ -2,5 +2,5 @@ import moment from "moment";
 
 export const lambdaHandler = async event => {
   const now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-  return `Current date and time is ${now} UTC. ${event.input}`;
+  return `Current date and time is ${now} UTC. ${event.input[0].text}`;
 };
